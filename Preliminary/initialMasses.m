5845955ac5abf772dd7ca2591f468b5f1b1a8013
SteBe01 <stefano3.belletti@mail.polimi.it>
2024-04-09 18:55:10 +0200

    Fix pressure losses

--- Preliminary/initialMasses.m
+++ Preliminary/initialMasses.m
@@ -4,14 +4,24 @@ OF = propellants.OF;
 rho_f = propellants.rho_rp1;
 rho_ox = propellants.rho_lox;
 
+P_i = geometry.P_start;
+P_f = geometry.P_min;
+
+[P_i_fu] = pressure_loss(P_i, rho_f);
+[P_i_ox] = pressure_loss(P_i, rho_ox);
+[P_f_fu] = pressure_loss(P_f, rho_f);
+[P_f_ox] = pressure_loss(P_f, rho_ox);
+
+P_i_fu = P_i_fu + P_i;
+P_i_ox = P_i_ox + P_i;
+P_f_fu = P_f_fu + P_f;
+P_f_ox = P_f_ox + P_f;
+
 V_conv = geometry.L_conv * pi/3 * (geometry.r_cc^2 + geometry.r_t^2 + geometry.r_cc*geometry.r_t);
 V_cc = geometry.L_cc * geometry.A_cc;
 V_occ = V_conv + V_cc;
 V = (geometry.L_conv+geometry.L_cc) * pi * (geometry.diameter_max/2)^2;
 V_tot = V - V_occ;
-
-P_i = geometry.P_start;
-P_f = geometry.P_min;
 
 new_OF = OF * rho_f/rho_ox; % volume
 
@@ -20,8 +30,8 @@ V_tank_ox = V_tank_fu*new_OF;
 
 % solve initial He volumes
 
-geometry.V_initial_He_fu = V_tank_fu * (P_f/P_i)^(1/propellants.k_He);
-geometry.V_initial_He_ox = V_tank_ox * (P_f/P_i)^(1/propellants.k_He);
+geometry.V_initial_He_fu = V_tank_fu * (P_f_fu/P_i_fu)^(1/propellants.k_He);
+geometry.V_initial_He_ox = V_tank_ox * (P_f_ox/P_i_ox)^(1/propellants.k_He);
 
 % solve final volumes of Ox and Fu
 
