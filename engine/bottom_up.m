80c89810dd6b4c3c2ccfb7c937a5613c6ba3dcfb
SteBe01 <stefano3.belletti@mail.polimi.it>
2024-04-11 14:22:32 +0200

    Fix bottom_up elseif @Leo777Deo

--- engine/bottom_up.m
+++ engine/bottom_up.m
@@ -19,11 +19,9 @@ for i = 1:const.N_iterations
     % Performances
     [engine, inj, comb_ch] = performances(prop, geom, engine, comb_ch, const,nozzle);
 
-    if  engine.T_real<const.T_id
+    if  engine.T_real < const.T_id
         engine.T = engine.T + (const.T_id-engine.T_real);
-
-    elseif engine.T_real>const.T_id
-        engine.T = engine.T + (const.T_id-engine.T_real);
+    else
         break
     end
 end
