31db07d13a33a6e53f32f5ef66dd2b9e12b00688
dtop <dantop912@gmail.com>
2024-04-11 11:42:52 +0200

    Added losses to topdown sim(not to stochastic)

--- engine/topdown_sim.m
+++ engine/topdown_sim.m
@@ -5,12 +5,31 @@ close all
 
 addpath(genpath('./functions'))
 
+% Data
 [engine, comb_ch, geom, prop, tank, nozzle, thermal, const] = get_data();
 comb_ch.P_start_real = comb_ch.P_start_id;
-[prop, nozzle] = combustion(prop, geom, nozzle, comb_ch);
-[geom, engine, nozzle] = nozzle_and_cc(prop, geom, engine, comb_ch, nozzle, const);
-[engine, inj, comb_ch] = performances(prop, geom, engine, comb_ch, const,nozzle);
-[tank, geom] = tanks(tank, prop, geom, engine, comb_ch, inj, thermal, const);
+
+% Combustion
+for i = 1:const.N_iterations
+    [prop, nozzle] = combustion(prop, geom, nozzle, comb_ch);
+
+    % Nozzle and Combustion Chamber
+    [geom, engine, nozzle] = nozzle_and_cc(prop, geom, engine, comb_ch, nozzle, const);
+
+    % Performances
+    [engine, inj, comb_ch] = performances(prop, geom, engine, comb_ch, const,nozzle);
+
+    if  engine.T_real<const.T_id
+        engine.T = engine.T + (const.T_id-engine.T_real);
+
+    elseif engine.T_real>const.T_id
+        engine.T = engine.T + (const.T_id-engine.T_real);
+        break
+    end
+end
+
+% Tanks
+    [tank, geom] = tanks(tank, prop, geom, engine, comb_ch, inj, thermal, const);
 
 k_he = prop.k_He; %helium monoatomic
 P_c = comb_ch.P_start_id; % [Pa]
